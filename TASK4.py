2c96dc42389de7f5ba965fb025c52591c392d0e7
SUSHMITHA-GANGADHAR <sushmithagsushmithag7@gmail.com>
2024-09-06 23:55:44 -0700

    Update TASK4.py

--- TASK4.py
+++ TASK4.py
@@ -1,3 +1,7 @@
+'''Analyze traffic accident data to identify patterns
+related to road conditions, weather, and time of day.
+Visualize accident hotspots and contributing factors.'''
+
 import pandas as pd
 import seaborn as sns
 import numpy as np
